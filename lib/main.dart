c5013d8f8768ded110bd455b3b4c6390ff8cfa98
ljwh1231 <ljwh123161@gmail.com>
2023-03-04 19:15:07 +0900

    랜덤한 4개 단어 셔플해서 그리드뷰 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'constant/words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,15 +17,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +27,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +34,95 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final List<String> _roundWords = [];
+  final List<List<String>> _wordGrid =
+      List.generate(4, (_) => List.generate(4, (_) => ''));
 
-  void _incrementCounter() {
+  void _setWordGrid() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      int idx = 0;
+      while (true) {
+        if (idx == 4) {
+          break;
+        }
+        int randIdx = Random().nextInt(words.length);
+        if (!_roundWords.contains(words[randIdx])) {
+          var newWord = words[randIdx].toUpperCase();
+          _roundWords.add(newWord);
+
+          for (var i = 0; i < 4; i++) {
+            _wordGrid[idx][i] = newWord.substring(i, i + 1);
+          }
+          idx++;
+        }
+      }
     });
   }
 
+  void _shuffleWordGrid() {
+    setState(() {
+      for (int i = 0; i < 4; i++) {
+        List<String> columnArr = [
+          _wordGrid[0][i],
+          _wordGrid[1][i],
+          _wordGrid[2][i],
+          _wordGrid[3][i]
+        ];
+        columnArr.shuffle();
+        for (int j = 0; j < 4; j++) {
+          _wordGrid[j][i] = columnArr[j];
+        }
+      }
+    });
+  }
+
+  // Widget shuffleWords() {
+  // return OutlinedButton(onPressed: _shuffleWordGrid, child: Text("SHUFFLE"));
+  // }
+
+  GridView makeWordGrid() {
+    _setWordGrid();
+    _shuffleWordGrid();
+    int axisCount = _wordGrid.length;
+    return GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: axisCount),
+        itemCount: _wordGrid[0].length * _wordGrid.length,
+        itemBuilder: _buildGridItems);
+  }
+
+  Widget _buildGridItems(BuildContext ctx, int index) {
+    if (_wordGrid.isEmpty) {
+      return const GridTile(
+
+        child: Center(child: Text('')),
+      );
+    }
+    int itemCount = _wordGrid.length;
+    int x = 0, y = 0;
+    x = (index / itemCount).floor();
+    y = (index % itemCount);
+
+    return GridTile(
+      child: Center(child: Text(_wordGrid[x][y])),
+    );
+  }
+
+  void lockRow() {}
+
+  void unlockRow() {}
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 400,
+            child: makeWordGrid(),
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
